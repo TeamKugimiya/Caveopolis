7a499844599342d1425908a85db5419180ac9569
Miku <26039249+xMikux@users.noreply.github.com>
2022-11-15 18:09:37 +0000

    新增發布腳本
    尚未完成發布版本的部分
    目前僅有開發版製作
    
    壓縮的提交歷史：
    - 修復錯誤
    - 再度修復一個 if 的錯誤
    - 希望這次修好
    - 測試這樣是否運作
    - 更改 sh 到 bash
    - 修復錯誤路徑

---
+++ .github/scripts/ModPacker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+############################
+### 模組包漢化補丁製作腳本 ###
+###### Made by Efina #######
+############################
+
+### 設定區 ###
+
+## 模組包名稱
+ModPackName=Caveopolis
+
+## 要建立進模組包漢化補丁中的陣列
+ConfigPath=(config kubejs patchouli_books LICENSE)
+
+### 腳本執行區 ###
+
+# 設置回主工作區
+cd "$GITHUB_WORKSPACE" || exit
+
+# 製作一個臨時資料夾
+mkdir workdir
+
+# 複製內容到臨時資料夾
+for path in "${ConfigPath[@]}"; do
+  cp -r "$path" workdir/
+done
+
+# 壓縮並製作模組包漢化補丁
+cd workdir || exit
+zip -r ../$ModPackName-模組包漢化補丁-$VERSION.zip *
+
+# 清理工作流程
+cd ..
+rm -r workdir
